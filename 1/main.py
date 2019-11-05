d4958eaadeaf8aa157e0bd57ff6e0d5bbe2d62e1
Michal Zylowski <zylowskimichal@gmail.com>
2019-11-05 19:10:54 +0100

    if
    Signed-off-by: Michal Zylowski <zylowskimichal@gmail.com>

---
+++ 1/main.py
@@ -0,0 +1,8 @@
+x = int(input("Elo typie, daj mi 5\n"))
+y = int(input("Elo typie, daj mi 2\n"))
+if x == 5 and y == 2:
+    print("Brawo! Podałeś poprawny kod!")
+elif x == 5 or y == 2:
+    print("Prawie dobrze.. :)")
+else:
+    print("No niestety. Błędny kod.")
